b7dcaba66f6721d9ba636c7d2c87e64a449d67aa
Vu Binh <22520129@gm.uit.edu.vn>
2025-02-04 11:12:01 +0700

    Exercise 001: Complete

---
+++ BaiTap/Bai_001/Bai_001.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	float x1, x2, y1, y2;
+	cout << "Nhap x1: ";
+	cin >> x1;
+	cout << "Nhap y1: ";
+	cin >> y1;
+	cout << "Nhap x2: ";
+	cin >> x2;
+	cout << "Nhap y2: ";
+	cin >> y2;
+	float kc = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+	cout << "Khoang cach giua 2 diem la: " << kc;
+	return 0;
+}
